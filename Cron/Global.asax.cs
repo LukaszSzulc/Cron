77bdcd2ec8b99df64da1c3a79394cdd02b9bcd6d
LukaszSzulc <lukasz.schultz@gmail.com>
2017-06-27 21:25:44 +0200

    Initial

--- Cron/Global.asax.cs
+++ Cron/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using FluentScheduler;
 
 namespace Cron
 {
@@ -16,6 +19,62 @@ namespace Cron
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<SampleInteraface>().As<ISampleInteraface>().InstancePerLifetimeScope();
+            builder.RegisterType<SampleJob>();
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinderProvider();
+
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            JobManager.JobFactory = new MyFactory(container.BeginLifetimeScope());
+            JobManager.Initialize(new MyRegistry());
+        }
+    }
+
+
+    public class MyFactory : IJobFactory
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public MyFactory(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public IJob GetJobInstance<T>() where T : IJob
+        {
+            using (var lifeTime = _lifetimeScope.BeginLifetimeScope())
+            {
+                return lifeTime.Resolve<T>();
+            }
+        }
+    }
+
+    public class MyRegistry : Registry
+    {
+        public MyRegistry()
+        {
+            Schedule<SampleJob>().ToRunNow().AndEvery(10).Seconds();
+        }
+    }
+
+    public class SampleJob : IJob
+    {
+        private readonly ISampleInteraface _interaface;
+
+        public SampleJob(ISampleInteraface interaface)
+        {
+            _interaface = interaface;
+        }
+        public void Execute()
+        {
+            _interaface.DoSomethingUseful();
         }
     }
 }
---
+++ Cron/ISampleInteraface.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Cron
+{
+    public interface ISampleInteraface
+    {
+        void DoSomethingUseful();
+    }
+
+    class SampleInteraface : ISampleInteraface
+    {
+        public void DoSomethingUseful()
+        {
+            Debug.Write("Konrad to pała");
+        }
+    }
+}
